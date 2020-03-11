d516093bba7818bf61043aba1ba9e504ec1f9efb
Luka Jerković <lukajerkovic@protonmail.com>
2020-03-11 14:20:23 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,19 +13,27 @@ $Env:NODE_OPTIONS = "max-old-space-size=8192"
 
 function GoAdmin { start-process pwsh –verb runAs }
 
+function Check-Modules {
+   Get-InstalledModule | foreach { $b = (find-module $_.name).version ; if ($b -ne $_.version) { Write-host "$($_.name) has an update from $($_.version) to $b" } } 
+}
+
+function Update-Modules {
+   Get-InstalledModule | Update-Module # -Force 
+}
+
 function Dotnet-Rebuild {
-     dotnet watch run  
+   dotnet watch run  
 }
 
 function Git-Commit {
-    param (
-        [string] $message
-    )
-    git add -A ; git commit -m $message
+   param (
+       [string] $message
+   )
+   git add -A ; git commit -m $message
 }
 
 function Git-Status {
-    git status
+   git status
 }
 
 function Dir-Icons {
@@ -37,3 +45,8 @@ Set-Alias gac Git-Commit
 Set-Alias -Name np -Value notepad++
 Set-Alias gs Git-Status
 Set-Alias lsi Dir-Icons
+Set-Alias pum Update-Modules
+Set-Alias pcm Check-Modules
+×
+Drag and Drop
+The image will be downloaded
