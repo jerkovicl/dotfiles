46d4870cead387c5a89f90d0be85d72f54c7eeae
Luka Jerković <lukajerkovic@protonmail.com>
2020-04-14 10:25:49 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,6 +21,27 @@ function Update-Modules {
    Get-InstalledModule | Update-Module # -Force 
 }
 
+function Set-PathVariable {
+   param (
+        [string]$AddPath,
+        [string]$RemovePath
+   )
+   $regexPaths = @()
+   if ($PSBoundParameters.Keys -contains 'AddPath'){
+        $regexPaths += [regex]::Escape($AddPath)
+   }
+
+   if ($PSBoundParameters.Keys -contains 'RemovePath'){
+        $regexPaths += [regex]::Escape($RemovePath)
+   }
+    
+   $arrPath = $env:Path -split ';'
+   foreach ($path in $regexPaths) {
+        $arrPath = $arrPath | Where-Object {$_ -notMatch "^$path\\?"}
+   }
+   $env:Path = ($arrPath + $addPath) -join ';'
+}
+
 function Dotnet-Rebuild {
    dotnet watch run  
 }
