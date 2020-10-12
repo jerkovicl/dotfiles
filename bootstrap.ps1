857b4ff8d144a86ef17a5691804cfa9e7a253055
Luka Jerković <lukajerkovic@protonmail.com>
2020-10-12 13:46:58 +0200

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,7 @@
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module cd-extras -Scope CurrentUser
+Install-Module Terminal-Icons -Scope CurrentUser
+Install-Module PsColor -Scope CurrentUser
+Install-Module PSFzf -Scope CurrentUser
