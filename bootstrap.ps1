2a2f7ec590202a92711364f82477f786199ff8aa
Luka Jerković <lukajerkovic@protonmail.com>
2020-10-16 14:40:32 +0200

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,6 @@
+# You could have the following to allow for scripts execution 
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+
 Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module posh-git -Scope CurrentUser
 Install-Module oh-my-posh -Scope CurrentUser
