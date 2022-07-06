89f350e8b1ac3f1a1cbf07264cb4f27a34ec1604
Luka Jerković <lukajerkovic@protonmail.com>
2022-07-06 13:33:11 +0200

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,44 +4,46 @@
 # Upgrade to oh-my-posh v3 Update-Module -Name oh-my-posh -AllowPrerelease -Scope CurrentUser
 # install: oh-my-posh winget install JanDeDobbeleer.OhMyPosh
 # upgrade: oh-my-posh winget upgrade JanDeDobbeleer.OhMyPosh
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
-Import-Module PSReadLine
+# ENV VARS
+
+$DefaultUser = "LukaJerković"
+$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$MyModulePath = "C:\Tools\Tools\utils\Modules"
+$env:PSModulePath = $env:PSModulePath + "$([System.IO.Path]::PathSeparator)$MyModulePath"
+$env:NPM_TOKEN = "xxxxx"
+$env:NODE_OPTIONS = "max-old-space-size=8192"
+[Environment]::SetEnvironmentVariable('FNM_DIR','C:\ProgramData\chocolatey\lib\fnm\tools')
+fnm env --use-on-cd | Out-String | Invoke-Expression # fnm node manager init
+
+# Modules import section
+
 Import-Module cd-extras
 Import-Module posh-git
-Import-Module oh-my-posh
-Import-Module Terminal-Icons
-#Import-Module Get-ChildItemColor
-#Import-Module PowerTab -ErrorAction SilentlyContinue
-#Import-Module PSColor
+Import-Module -Name Terminal-Icons
+
 # PSReadLine settings
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -Colors @{ 
- InlinePrediction = '#39A3F9' 
- ListPredictionSelected = '#39A3F9'
-} 
-#ListPrediction 
-Set-PSReadLineOption -PredictionViewStyle ListView
-#Set-PSReadLineOption -EditMode Windows
-Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+$PSReadLineOptions = @{
+    PredictionSource = "HistoryAndPlugin"
+    PredictionViewStyle = "ListView"
+    EditMode = "Windows"
+    HistoryNoDuplicates = $true
+    HistorySearchCursorMovesToEnd = $true
+    Colors = @{
+     "InlinePrediction" = '#39A3F9'
+     "ListPredictionSelected" = '#39A3F9'
+    }
+}
+Set-PSReadLineOption @PSReadLineOptions
+Remove-PSReadlineKeyHandler 'Ctrl+r'
+Remove-PSReadlineKeyHandler 'Ctrl+t'
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d,Ctrl+c' -Function CaptureScreen
 #Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 #Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
-Remove-PSReadlineKeyHandler 'Ctrl+r'
-Remove-PSReadlineKeyHandler 'Ctrl+t'
 Import-Module PSFzf
-Set-PoshPrompt -Theme  ~/.go-my-posh.json # for oh-my-posh v3
-Set-Theme Paradox #Set-Theme Agnoster
-Set-TerminalIconsColorTheme -Name DevBlackOps
-Import-Module DockerCompletion
-Import-Module NPMTabCompletion
-#$global:PSColor.File.Executable.Color = 'Blue'
-#$ThemeSettings.GitSymbols.BranchSymbol = [char]::ConvertFromUtf32(0xE0A0)
-#unicorn symbol [char]::ConvertFromUtf32(0x01F984)
-$ThemeSettings.PromptSymbols.StartSymbol = [char]::ConvertFromUtf32(0x0001F525) 
-$ThemeSettings.PromptSymbols.PromptIndicator = [char]::ConvertFromUtf32(0x01F680) +[char]::ConvertFromUtf32(0x276F)
-$DefaultUser = 'A687301'
-$Env:NODE_OPTIONS = "max-old-space-size=8192"
+
+# OH-MY-POSH INIT
+oh-my-posh --init --shell pwsh --config ~/.go-my-posh.json | Invoke-Expression
 
 # winget autocomplete
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
@@ -65,37 +67,131 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 function GoAdmin { start-process pwsh –verb runAs }
 
 function Check-Modules {
-   Get-InstalledModule | foreach { $b = (find-module $_.name).version ; if ($b -ne $_.version) { Write-host "$($_.name) has an update from $($_.version) to $b" } } 
+   Get-InstalledModule | foreach { $b = (find-module $_.name).version ; if ($b -ne $_.version) { Write-host "$($_.name) has an update from $($_.version) to $b" } }
 }
 
 function Update-Modules {
-   Get-InstalledModule | Update-Module # -Force 
-}
-
-function Set-PathVariable {
-   param (
-        [string]$AddPath,
-        [string]$RemovePath
-   )
-   $regexPaths = @()
-   if ($PSBoundParameters.Keys -contains 'AddPath'){
-        $regexPaths += [regex]::Escape($AddPath)
-   }
-
-   if ($PSBoundParameters.Keys -contains 'RemovePath'){
-        $regexPaths += [regex]::Escape($RemovePath)
-   }
-    
-   $arrPath = $env:Path -split ';'
-   foreach ($path in $regexPaths) {
-        $arrPath = $arrPath | Where-Object {$_ -notMatch "^$path\\?"}
-   }
-   $env:Path = ($arrPath + $addPath) -join ';'
+   Get-InstalledModule | Update-Module # -Force -AllowPrerelease
 }
 
 function Dotnet-Rebuild {
-   dotnet watch run  
+   dotnet watch run
 }
+
+# Git
+
+function gb {
+    git checkout -b $args
+}
+
+function gbt ([string] $taskid) {
+    git checkout -b "task/$taskid"
+}
+
+function gs {
+    git checkout $args
+    git pull
+}
+
+function gmaster {
+    gs 'master'
+}
+
+function gmain {
+    gs 'main'
+}
+
+function gdev {
+    gs 'develop'
+}
+
+function gco {
+    git add .
+    git commit -m $args
+}
+
+function gfeat {
+    if($null -eq $args[1]) {
+        gco "feat: $($args[0])"
+    }else {
+        gco "feat($($args[0])): $($args[1])"
+    }
+}
+
+function gfix {
+    if($null -eq $args[1]) {
+        gco "fix: $($args[0])"
+    }else {
+        gco "fix($($args[0])): $($args[1])"
+    }
+}
+
+function gtest {
+    if($null -eq $args[1]) {
+        gco "test: $($args[0])"
+    }else {
+        gco "test($($args[0])): $($args[1])"
+    }
+}
+
+function gdocs {
+    if($null -eq $args[1]) {
+        gco "docs: $($args[0])"
+    }else {
+        gco "docs($($args[0])): $($args[1])"
+    }
+}
+
+function gstyle {
+    if($null -eq $args[1]) {
+        gco "style: $($args[0])"
+    }else {
+        gco "style($($args[0])): $($args[1])"
+    }
+}
+
+function grefactor {
+    if($null -eq $args[1]) {
+        gco "refactor: $($args[0])"
+    }else {
+        gco "refactor($($args[0])): $($args[1])"
+    }
+}
+
+function gperf {
+    if($null -eq $args[1]) {
+        gco "perf: $($args[0])"
+    }else {
+        gco "perf($($args[0])): $($args[1])"
+    }
+}
+
+function gchore {
+    if($null -eq $args[1]) {
+        gco "chore: $($args[0])"
+    }else {
+        gco "chore($($args[0])): $($args[1])"
+    }
+}
+
+function gpu {
+    git pull
+}
+
+function goops {
+    git add .
+    git commit --amend --no-edit
+}
+
+function gfp {
+    git push --force-with-lease
+}
+
+function gr {
+    git reset --hard
+    git clean -f -d
+}
+
 
 function Git-Commit {
    param (
@@ -111,6 +207,8 @@ function Git-Status {
 function GitLogPretty {
   git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --all
 }
+
+# Utils
 
 function Dir-Icons {
    Get-ChildItem | Format-Wide
@@ -133,9 +231,9 @@ function Add-Path {
     .SYNOPSIS
       Adds a Directory to the Current Path
     .DESCRIPTION
-      Add a directory to the current path.  This is useful for 
-      temporary changes to the path or, when run from your 
-      profile, for adjusting the path within your powershell 
+      Add a directory to the current path.  This is useful for
+      temporary changes to the path or, when run from your
+      profile, for adjusting the path within your powershell
       prompt.
     .EXAMPLE
       Add-Path -Directory "C:\Program Files\Notepad++"
@@ -173,9 +271,21 @@ function Add-Path {
   }
 }
 
+#C:\Tools\Tools\utils\start_docker.ps1
+function StartWSLDocker {
+  $ip = (wsl sh -c "hostname -I").Split(" ")[0]
+  netsh interface portproxy add v4tov4 listenport=2375 connectport=2375 connectaddress=$ip
+  wsl sh -c "sudo dockerd -H tcp://$ip"
+}
+
+function WriteNodeVersionForFnm {
+  node --version > .node-version
+}
+
+# Aliases
+
 Add-Path -Directory "C:\Program Files\Notepad++"
 Set-Alias edit notepad++.exe
-
 Set-Alias .. GoBack
 Set-Alias -Name np -Value notepad++
 Set-Alias dwr Dotnet-Rebuild
@@ -185,8 +295,7 @@ Set-Alias glp GitLogPretty
 Set-Alias ls Dir-Icons
 Set-Alias pum Update-Modules
 Set-Alias pcm Check-Modules
-Set-Alias gci Get-ComputerInfo
 Set-Alias myip GetMyIp
 Set-Alias isesm IsNpmPackageEsm
-# setup fnm as node manager
-fnm env --use-on-cd | Out-String | Invoke-Expression
+Set-Alias swsld StartWSLDocker
+Set-Alias wnv WriteNodeVersionForFnm
